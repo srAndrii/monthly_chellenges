557a1a28bec7263e8133b7a803a0c58b55efdc0b
srAndrii <55akif5548@gmail.com>
2023-02-19 18:16:39 +0200

    Work with views and urls

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Learn Django for at least 20 minutes every day!"
+}
+
+
+def january(request):
+    return HttpResponse("Eat no meat for the entire month!")
+
+
+def february(request):
+    return HttpResponse("Walk for at least 20 minutes every day!")
+
+
+def march(request):
+    return HttpResponse("Learn Django for at least 20 minutes every day!")
+
+
+
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This is not supported!")
