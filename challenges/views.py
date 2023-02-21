8181ddc92539386cda76896a3e96bd48a4a4ae7b
srAndrii <55akif5548@gmail.com>
2023-02-21 09:46:11 +0200

    Work with 404page

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect, HttpResponseNotFound
 from django.urls import reverse
 from django.template.loader import render_to_string
 
@@ -49,4 +49,4 @@ def monthly_challenge(request, month):
             'month_name': month
         })
     except:
-        return HttpResponseNotFound("<h1>This is not supported!</h1>")
+        raise Http404()
